e7f6e044c7f9349b65984e713f99d35bb097856b
cpsievert <cpsievert@users.noreply.github.com>
2022-05-23 20:52:06 +0000

    `yarn build` (GitHub Actions)

--- srcts/types/src/bindings/registry.d.ts
+++ srcts/types/src/bindings/registry.d.ts
@@ -1,23 +1,23 @@
 import { Callbacks } from "../utils/callbacks";
 interface BindingBase {
-  name: string;
+    name: string;
 }
 interface BindingObj<Binding> {
-  binding: Binding;
-  priority: number;
-  name?: string;
+    binding: Binding;
+    priority: number;
+    name?: string;
 }
 declare class BindingRegistry<Binding extends BindingBase> {
-  name: string;
-  bindings: Array<BindingObj<Binding>>;
-  bindingNames: {
-    [key: string]: BindingObj<Binding>;
-  };
-  registerCallbacks: Callbacks;
-  register(binding: Binding, bindingName: string, priority?: number): void;
-  onRegister(fn: () => void, once?: boolean): void;
-  setPriority(bindingName: string, priority: number): void;
-  getPriority(bindingName: string): number | false;
-  getBindings(): Array<BindingObj<Binding>>;
+    name: string;
+    bindings: Array<BindingObj<Binding>>;
+    bindingNames: {
+        [key: string]: BindingObj<Binding>;
+    };
+    registerCallbacks: Callbacks;
+    register(binding: Binding, bindingName: string, priority?: number): void;
+    onRegister(fn: () => void, once?: boolean): void;
+    setPriority(bindingName: string, priority: number): void;
+    getPriority(bindingName: string): number | false;
+    getBindings(): Array<BindingObj<Binding>>;
 }
 export { BindingRegistry };
